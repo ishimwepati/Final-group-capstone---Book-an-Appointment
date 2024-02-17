2a84b5839eae89470f9301a13ca950830d35ecc6
mryeminthu <dhammaarchive@hotmail.com>
2024-02-17 15:02:11 +0630

    Add routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavigationPanel from './components/NavigationPanel';
 import MotorcycleList from './components/MotorcycleList';
-import './App.css'; // Create an App.css file for styling
+import Reservations from './components/reservations/Reservations';
 
 const App = () => (
   <Router>
@@ -11,6 +10,7 @@ const App = () => (
       <NavigationPanel />
       <Routes>
         <Route path="/" element={<MotorcycleList />} />
+        <Route path="/my-reservations" element={<Reservations />} />
       </Routes>
     </div>
   </Router>

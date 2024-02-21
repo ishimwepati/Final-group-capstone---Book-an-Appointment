6611271e4ba79a527756e7f7016ede46ea5d9d19
mryeminthu <dhammaarchive@hotmail.com>
2024-02-21 15:50:18 +0630

    Add bootstrap for Signin.jsx

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -5,9 +5,9 @@ import { login } from '../redux/userSlice';
 
 const SignIn = () => {
   const [credential, setCredential] = useState({ email: '', password: '' });
-  const navegate = useNavigate();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { currentUser, message } = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
 
   if (currentUser) return <Navigate to="/motorcycles" />;
 
@@ -17,47 +17,54 @@ const SignIn = () => {
   };
 
   return (
-    <form method="post" onSubmit={handleLogin}>
-      <div className="flexV">
-        <label htmlFor="email">
-          Email
-          <br />
+    <div className="container mt-5">
+      <form
+        className="card p-4"
+        style={{ border: '1.4px solid #000', borderRadius: '7px' }}
+        method="post"
+        onSubmit={handleLogin}
+      >
+        <h2 className="text-center mb-4">Sign in</h2>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
           <input
             type="email"
+            className="form-control"
             name="email"
             id="email"
             onChange={(e) => setCredential({ ...credential, email: e.target.value })}
             required
           />
-        </label>
-      </div>
+        </div>
 
-      <div className="flexV">
-        <label htmlFor="password">
-          Password
-          <br />
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
           <input
             type="password"
+            className="form-control"
             name="password"
             id="password"
             onChange={(e) => setCredential({ ...credential, password: e.target.value })}
             required
           />
-        </label>
-      </div>
+        </div>
 
-      <div>
-        <button type="submit">Login</button>
-        <button
-          type="button"
-          onClick={() => {
-            navegate('/signup');
-          }}
-        >
-          Sign up
-        </button>
-      </div>
-    </form>
+        <div className="d-flex justify-content-between">
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => {
+              navigate('/signup');
+            }}
+          >
+            Sign up
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 

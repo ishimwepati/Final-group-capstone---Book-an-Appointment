1b192f73535d95ffb31210db682eabe86ba95b79
WazaCode <wazacode@WazaCodes-MacBook-Pro.local>
2024-02-21 21:09:05 +0300

    Add Motorcycle styles done

--- src/components/delete2/AddItem.jsx
+++ src/components/delete2/AddItem.jsx
@@ -1,16 +1,82 @@
-import React from 'react';
-import './Delete.css';
+// AddItem.jsx
 
-const AddItem = () => (
-  <div className="delete-container">
-    <h2>Add Motorcycle</h2>
-    <div className="delete-div">
-      <p>Motorcycle name</p>
-      <button type="button" className="delete-button">
-        AddMotocycle
-      </button>
+import React, { useState } from 'react';
+import './AddItem.css';
+
+const AddItem = () => {
+  const [motorcycleData, setMotorcycleData] = useState({
+    name: '',
+    color: '',
+    weight: '',
+    madeDate: '',
+    madeCountry: '',
+  });
+
+  const handleAddMotorcycle = () => {
+    setMotorcycleData({
+      name: '',
+      color: '',
+      weight: '',
+      madeDate: '',
+      madeCountry: '',
+    });
+  };
+
+  return (
+    <div className="add-container">
+      <h2>Add Motorcycle</h2>
+      <div className="add-div">
+        <label htmlFor="motorcycleName">
+          Motorcycle Name:
+          <input
+            type="text"
+            id="motorcycleName"
+            value={motorcycleData.name}
+            onChange={(e) => setMotorcycleData({ ...motorcycleData, name: e.target.value })}
+          />
+        </label>
+        <label htmlFor="motorcycleColor">
+          Motorcycle Color:
+          <input
+            type="text"
+            id="motorcycleColor"
+            value={motorcycleData.color}
+            onChange={(e) => setMotorcycleData({ ...motorcycleData, color: e.target.value })}
+          />
+        </label>
+        <label htmlFor="motorcycleWeight">
+          Motorcycle Weight:
+          <input
+            type="text"
+            id="motorcycleWeight"
+            value={motorcycleData.weight}
+            onChange={(e) => setMotorcycleData({ ...motorcycleData, weight: e.target.value })}
+          />
+        </label>
+        <label htmlFor="motorcycleMadeDate">
+          Motorcycle Made Date:
+          <input
+            type="date"
+            id="motorcycleMadeDate"
+            value={motorcycleData.madeDate}
+            onChange={(e) => setMotorcycleData({ ...motorcycleData, madeDate: e.target.value })}
+          />
+        </label>
+        <label htmlFor="motorcycleMadeCountry">
+          Motorcycle Made Country:
+          <input
+            type="text"
+            id="motorcycleMadeCountry"
+            value={motorcycleData.madeCountry}
+            onChange={(e) => setMotorcycleData({ ...motorcycleData, madeCountry: e.target.value })}
+          />
+        </label>
+        <button type="button" className="add-button" onClick={handleAddMotorcycle}>
+          Add Motorcycle
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default AddItem;

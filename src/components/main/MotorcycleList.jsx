d924e934e3a161a5933221c4ec1e75bdf6b22764
mryeminthu <dhammaarchive@hotmail.com>
2024-02-24 10:11:14 +0630

    Add styles in main folder

--- src/components/main/MotorcycleList.jsx
+++ src/components/main/MotorcycleList.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, Link } from 'react-router-dom';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import { getMotorcycles } from '../../redux/motorcycleSlice';
 import NavigationPanel from '../NavigationPanel';
@@ -11,6 +11,9 @@ const MotorcycleList = () => {
   const authorization = useSelector((state) => state.user.requestHeader);
   const dispatch = useDispatch();
   const motorcycles = useSelector((state) => state.motorcycle.motorcycles);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   useEffect(() => {
     if (authorization) {
       dispatch(getMotorcycles(authorization));
@@ -19,35 +22,56 @@ const MotorcycleList = () => {
 
   if (!currentUser) return <Navigate to="/login" />;
 
+  const handleLeftArrowClick = () => {
+    setCurrentIndex((prevIndex) => Math.max(prevIndex - 3, 0));
+  };
+
+  const handleRightArrowClick = () => {
+    setCurrentIndex((prevIndex) => Math.min(prevIndex + 3, motorcycles.length - 3));
+  };
+
   return (
     <>
       <NavigationPanel />
       <div className="motorcycle-list-container">
         <div className="carousel-container">
-          <FaChevronLeft className="carousel-arrow" />
+          <FaChevronLeft
+            className="carousel-arrow"
+            onClick={handleLeftArrowClick}
+          />
           {motorcycles && motorcycles.length === 0 ? (
             <p className="no-items-message">No motorcycles found</p>
           ) : (
             <ul className="motorcycle-list">
               {motorcycles
-                && motorcycles.map((motorcycle) => (
-                  <li key={motorcycle.id} className="motorcycle-item">
-                    <div className="motorcycle-details">
-                      <h3 className="motorcycle-name">
-                        {motorcycle.motorcycle_name}
-                      </h3>
-                      <p className="motorcycle-description">
-                        {motorcycle.description}
-                      </p>
-                      <p className="motorcycle-description">
-                        {motorcycle.image}
-                      </p>
-                    </div>
-                  </li>
-                ))}
+                && motorcycles
+                  .slice(currentIndex, currentIndex + 3)
+                  .map((motorcycle) => (
+                    <li key={motorcycle.id} className="motorcycle-item">
+                      {motorcycle && (
+                        <Link
+                          to={{
+                            pathname: `/details/${motorcycle.id}`,
+                            state: { motorcycle },
+                          }}
+                          className="motorcycle-link"
+                        >
+                          <div className="motorcycle-details">
+                            <h3 className="motorcycle-name">
+                              {motorcycle.make}
+                            </h3>
+                            {motorcycle.image}
+                          </div>
+                        </Link>
+                      )}
+                    </li>
+                  ))}
             </ul>
           )}
-          <FaChevronRight className="carousel-arrow" />
+          <FaChevronRight
+            className="carousel-arrow"
+            onClick={handleRightArrowClick}
+          />
         </div>
       </div>
     </>

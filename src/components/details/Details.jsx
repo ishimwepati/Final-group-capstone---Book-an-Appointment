2eb85fa9ed15e1ccfe60f17f6ef94e6ae9293b68
mryeminthu <dhammaarchive@hotmail.com>
2024-02-24 16:07:21 +0630

    Fetch data for the details page

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,39 +1,71 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import { getMotorcycles } from '../../redux/motorcycleSlice';
+import NavigationPanel from '../NavigationPanel';
 import './Details.css';
 
-const Details = () => (
-  <div className="details-container">
-    <h2>Motorcycle name</h2>
-    <div className="details-div">
-      <table className="table table-bordered table-striped">
-        <tbody>
-          <tr>
-            <td>Name</td>
-            <td>Motorcycle name</td>
-          </tr>
-          <tr>
-            <td>Description</td>
-            <td>
-              Contrary to popular belief, Lorem Ipsum is not simply random text.
-              It has roots in a piece of classical Latin literature from 45 BC,
-              making it over 2000 years old.
-            </td>
-          </tr>
-          <tr>
-            <td>Fee</td>
-            <td>2</td>
-          </tr>
-          <tr>
-            <td>Duration</td>
-            <td>2</td>
-          </tr>
-        </tbody>
-      </table>
-      <button type="button" className="reservation-button">
-        Make Reservation
-      </button>
-    </div>
-  </div>
-);
+const Details = () => {
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.user.currentUser);
+  const motorcycles = useSelector((state) => state.motorcycle.motorcycles);
+
+  useEffect(() => {
+    if (!currentUser) {
+      // Handle authentication if needed
+      return;
+    }
+
+    // Check if motorcycles are already loaded, if not, fetch them
+    if (motorcycles.length === 0) {
+      dispatch(getMotorcycles(currentUser.token));
+    }
+  }, [currentUser, dispatch, motorcycles]);
+
+  // Extract motorcycle ID from the URL
+  const { pathname } = location;
+  const motorcycleId = pathname.split('/details/')[1];
+
+  // Find the corresponding motorcycle using the ID
+  const motorcycle = motorcycles.find((m) => m.id.toString() === motorcycleId);
+
+  // Log the motorcycle data to the console for debugging
+  useEffect(() => {
+    console.log('Motorcycle Data:', motorcycle);
+  }, [motorcycle]);
+
+  return (
+    <>
+      <NavigationPanel />
+      <div className="details-container">
+        <h2>Motorcycle Details</h2>
+        <div className="details-div">
+          {motorcycle && (
+            <table className="table table-bordered table-striped">
+              <tbody>
+                <tr>
+                  <td>Motorcycle Name</td>
+                  <td>{motorcycle.make}</td>
+                </tr>
+                <tr>
+                  <td>Description</td>
+                  <td>{motorcycle.description}</td>
+                </tr>
+                <tr>
+                  <td>Color</td>
+                  <td>{motorcycle.color}</td>
+                </tr>
+              </tbody>
+            </table>
+          )}
+          <button type="button" className="reservation-button">
+            Make Reservation
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Details;
